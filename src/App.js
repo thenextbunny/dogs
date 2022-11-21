ecc7f64cc64a78802d18990e4140fea90bfff09a
Vitor <vitorcoelhosilvat@gmail.com>
2022-11-21 14:45:11 -0300

    :up:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+// React router dom
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home/Home";
+
 const App = () => {
-	return;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
